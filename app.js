f3f4af2ad6f67ee17580a6db3e700af2080aba66
jennifer cox <jenncox97@gmail.comgit config --global user.email jenncox97@gmail.com>
2023-09-19 21:52:12 -0400

    done

--- app.js
+++ app.js
@@ -2,6 +2,43 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for(let i=0; i<=100; i++){
+    if (i%2==0 ) {
+        continue;
+    } else {console.log(i);
+}
+    }
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for(let i=0; 1<=100; i++){
+    console.log(i)
+    if (i%3==0) {
+        console.log("fizz"); 
+    } else if (i%5==0) {
+    console.log("buzz");
+    } else (i%3==0&&i%5==0)
+        console.log("fizzbuzz");
+    }
+
+
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+let i=1
+while(i<100){
+    if(i%2==0) {
+        console.log(i);
+    }
+    i++;
+}
+
+let x=1
+do{
+    if(x%2==0) {
+        console.log(x);
+    }
+    x++;
+} while(x<100);
+
+//Exercise 4
+
